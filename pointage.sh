99b02ea55481e99f496916096c133e0f28a19699
Fabouin <66368600+Fabouin@users.noreply.github.com>
2022-02-15 09:09:48 +0100

    Update pointage.sh

--- pointage.sh
+++ pointage.sh
@@ -5,10 +5,10 @@
 
 username="-------------------------------username---------------"   #mettre le mot de passe et le user entre les guillement
 password="------------------------------password-------------------"
-
+jour=$(date +%d/%m/%Y)
 
 #1-------ouverture du site pour recuperer le cookie de session.
-echo "lancement de la requet http"
+echo $jour': lancement de la requet http' >> log.txt
 curl -s -c .cookie.txt https://www.formation-occ.com/index.php > /dev/null
 
 #2---------connection avec l'identifiant et recuperation du token
@@ -23,10 +23,10 @@ curl  --progress-bar -b .cookie.txt  https://www.formation-occ.com/index.php  > 
 name=$(grep "user_name" .checkname | cut -d'>' -f5 | cut -d'<' -f1 );
 #echo $name
 if [ -z "$name" ];then
-   echo "login error"
+   echo "login error" >> log.txt
 else 
     
-    echo "login succes as $name"
+    echo "login succes as $name" >> log.txt
 fi 
 #grep "form_connexion form_required" ./.checkname 
 
@@ -36,13 +36,13 @@ curl -L --progress-bar -b .cookie.txt "https://www.formation-occ.com/insert_atte
 point=$(grep "pointage" .checkname | cut -d'>' -f2 | cut -d'<' -f1);
 #echo $point
 if [ -z "$point" ];then
-   echo "erreur de pointage la page n'a pas ete ouverte"
+   echo "erreur de pointage la page n'a pas ete ouverte" >> log.txt
 else 
     
-    echo "$point"
+    echo "$point" >> log.txt
 fi 
 #grep "form_connexion form_required" ./.checkname2
-
+echo -e "\n" >> log.txt
 #-----nettoyage des cookies
 rm -f .checkname
 rm -f .cookie.txt
